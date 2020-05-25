f258a1c778f162ce04dc7631e559ccc5290757c4
David Cusi <robodavcuqupe@gmail.com>
2020-05-25 15:02:35 -0400

    Agregue las funciones publicarImagen y subir imagen

--- public/js/publicar/Publicar.js
+++ public/js/publicar/Publicar.js
@@ -20,4 +20,24 @@ class Publicar{
             M.toast({html: `Error al guardar la publicación error: ${error}`,displayLength: 1000});
         })
     }
+    subirImagen(file, uid){
+        const refStorage = firebase.storage().ref(`imagenesPublicadas/${uid}/${file.name}`);
+        const task = refStorage.put(file);
+        task.on('state_changed', snapshot => {
+            const porcentaje = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+            document.querySelector('.porcentaje').style.cssText = `width: ${porcentaje}%;`;
+            
+        }, err => {
+            M.toast({html: `Error al guardar en la base de datos ${err}`,displayLength: 2000});
+        }, () => {
+            task.snapshot.ref.getDownloadURL()
+                .then(url => {
+                    sessionStorage.setItem('imgNewPost', url)
+                    M.toast({html: `URL: ${url}`,displayLength: 2000});
+                })
+                .catch(err => {
+                    M.toast({html: `Error obteniendo la URL: ${err}`,displayLength: 2000});
+                })
+        })
+    }
 }
--- public/js/publicar/publicarController.js
+++ public/js/publicar/publicarController.js
@@ -2,6 +2,7 @@ const hacerPublicacion = document.querySelector('#hacerPublicacion');
 const publicarImagen = document.querySelector('#publicarImagen');
 const btnPublicarImagen = document.querySelector('#btnPublicarImagen');
 
+const btnUploadImagen = document.querySelector('#file-upload');
 
 hacerPublicacion.addEventListener('click', function(){
     const user = firebase.auth().currentUser;
@@ -24,5 +25,50 @@ publicarImagen.addEventListener('click', function(){
 });
 btnPublicarImagen.addEventListener('click', function(){
     event.preventDefault();
-    M.toast({html: `publicado`,displayLength: 1000});
+    const publicar = new Publicar();
+    
+    const user = firebase.auth().currentUser
+    console.log(user.uid)
+    if(user == null){
+        M.toast({html: `Debes de iniciar sesión para publicar`,displayLength: 1000});
+        return 
+    }
+    const titulo = document.querySelector('#imagenTitulo').value;
+    const autor = document.querySelector('#imagenAutor').value;
+    const descripcion = document.querySelector('#imagenDescripcion').value;
+    const contenido = document.querySelector('#imagenContenido').value;
+    const imagenLink = sessionStorage.getItem('imgNewPost') == 'null'
+        ? null
+        : sessionStorage.getItem('imgNewPost');
+    
+    publicar.publicarImagen(
+        user.uid,
+        user.email,
+        titulo,
+        autor,
+        descripcion,
+        contenido,
+        imagenLink
+    )
+    .then(resp => {
+        M.toast({html: `Image publicada correctamente`,displayLength: 2000});
+        const modal = document.querySelector('#modalImagen')
+        const modalInstancia = M.Modal.getInstance(modal);
+        modalInstancia.close()
+    })
+    .catch(err => {
+        M.toast({html: `Error: ${err}`,displayLength: 2000});
+    })
 })
+
+btnUploadImagen.addEventListener('change', function(e){
+    const user = firebase.auth().currentUser;
+    if(user == null){
+        M.toast({html: `Debes de iniciar sesión para publicar`,displayLength: 1000});
+        return 
+    }
+    const file = e.target.files[0];
+    M.toast({html: `Archivo ${file}`,displayLength: 2000});
+    const publicar = new Publicar();
+    publicar.subirImagen(file, user.uid);
+}, false)
